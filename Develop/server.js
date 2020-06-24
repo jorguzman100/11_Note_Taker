a1c59b1c0ab3f7755ca780feb48d00e3c2ef6a06
Jorge Guzman <jorguzman100@gmail.com>
2020-06-24 17:11:26 -0500

    GET /notes route defined

--- Develop/server.js
+++ Develop/server.js
@@ -26,14 +26,14 @@ app.use(express.static(__dirname + '/public')); // For everything is running in 
 
 // GET `*` - Should return the `index.html` file
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
     console.log('Root: /');
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // GET `/notes` - Should return the `notes.html` file.
 app.get('/notes', (req, res) => {
-    console.log('Root: /notes');
     res.sendFile(path.join(__dirname, './public/notes.html'));
+    console.log('Root: /notes');
 });
 
 // API Routes
