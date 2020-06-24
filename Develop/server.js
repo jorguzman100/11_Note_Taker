dd49101f6686cad38995aa52607b997f2e37fe1b
Jorge Guzman <jorguzman100@gmail.com>
2020-06-24 18:30:04 -0500

    Full functionality working

--- Develop/server.js
+++ Develop/server.js
@@ -2,15 +2,10 @@
 const express = require('express');
 const { urlencoded } = require('body-parser');
 const path = require("path");
-// const { v4: uuidv4 } = require('uuid');
-
-// Database
-// The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
-let db = require('./db/db.json');
 const fs = require('fs');
 
-
-
+// Database
+let db = require('./db/db.json');
 
 // Port
 const PORT = process.env.PORT || 3000;
@@ -18,32 +13,28 @@ const PORT = process.env.PORT || 3000;
 // Create server / Activate Express
 const app = express();
 
-
-// Middleware for post methods
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/public')); // For everything is running in the frontend, use this folder
+// For everything is running in the frontend, use this folder
+app.use(express.static(__dirname + '/public')); 
 
 
 // HTML Routes
 
-// GET `*` - Should return the `index.html` file
 app.get('/', (req, res) => {
-    console.log('Root: /');
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// GET `/notes` - Should return the `notes.html` file.
 app.get('/notes', (req, res) => {
-    console.log('Root: /notes');
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+
 // API Routes
 
-// GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get(`/api/notes`, (req, res) => {
-    console.log('Database: ', db);
+    // Read the `db.json` file and return all saved notes as JSON
     res.json(db);
 });
 
@@ -57,11 +48,10 @@ app.post('/api/notes', (req, res) => {
         }
     });
     res.json(db);
-    console.log('Database: ', db);
 });
 
 // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. */
-app.get(`/api/notes/:id`, (req, res) => {
+app.delete(`/api/notes/:id`, (req, res) => {
     let id = req.params.id;
     console.log('id: ', id);
     for (var i = 0; i < db.length; i++) {
