4a1167f540e62b25a0dfa626502a0491e251ce5d
Jorge Guzman <jorguzman100@gmail.com>
2020-06-24 17:14:15 -0500

    POST /api/notes API route defined

--- Develop/server.js
+++ Develop/server.js
@@ -40,9 +40,8 @@ app.get('/notes', (req, res) => {
 
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get(`/api/notes`, (req, res) => {
-    
-    res.json(db);
     console.log('Database: ', db);
+    res.json(db);
 });
 
 // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
@@ -54,8 +53,8 @@ app.post('/api/notes', (req, res) => {
             throw err;
         }
     });
-    console.log('Database: ', db);
     res.json(db);
+     console.log('Database: ', db);
 });
 
 // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file. */
