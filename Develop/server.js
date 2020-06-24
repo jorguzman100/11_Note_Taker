3b8c9c415e3f0347712fec7a77ef90d94e08d7ae
Jorge Guzman <jorguzman100@gmail.com>
2020-06-24 17:13:09 -0500

    GET /api/notes API route defined

--- Develop/server.js
+++ Develop/server.js
@@ -32,16 +32,17 @@ app.get('/', (req, res) => {
 
 // GET `/notes` - Should return the `notes.html` file.
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
     console.log('Root: /notes');
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // API Routes
 
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get(`/api/notes`, (req, res) => {
-    console.log('Database: ', db);
+    
     res.json(db);
+    console.log('Database: ', db);
 });
 
 // POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
